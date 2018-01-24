3217b6b208a025878b44f2f805d8bcdc4de60a40
Michael Ruhl <michael.ruhl@novatec-gmbh.de>
2018-01-24 10:47:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# android_emulator plugin
+
+[![fastlane Plugin Badge](https://rawcdn.githack.com/fastlane/fastlane/master/fastlane/assets/plugin-badge.svg)](https://rubygems.org/gems/fastlane-plugin-android_emulator)
+
+## Getting Started
+
+This project is a [_fastlane_](https://github.com/fastlane/fastlane) plugin. To get started with `fastlane-plugin-android_emulator`, add it to your project by running:
+
+```bash
+fastlane add_plugin android_emulator
+```
+
+## About android_emulator
+
+Creates and starts a new Android Emulator (AVD)
+With additional features:
+
+* set location
+* set demo-mode (great for Screengrab 😀)
+
+## Example
+
+```ruby
+android_emulator(
+	location: '9.1808 48.7771',
+	package: "system-images;android-24;google_apis;x86_64",
+	demo_mode: true,
+	sdk_dir: "PATH_TO_SDK"
+)
+```
+
+Or you can use it with our [Android SDK Update Plugin](https://github.com/NovaTecConsulting/fastlane-plugin-android_sdk_update) 
+
+
+```ruby
+ENV["AVD_PACKAGE"] = "system-images;android-24;google_apis;x86_64"
+
+# installs the emulator and system-image
+ android_sdk_update(
+	additional_packages: [
+		ENV["AVD_PACKAGE"],
+        "emulator"]
+)
+
+android_emulator(
+ 	location: '9.1808 48.7771'
+)
+```
+
+## Run tests for this plugin
+
+To run both the tests, and code style validation, run
+
+```
+rake
+```
+
+To automatically fix many of the styling issues, use
+```
+rubocop -a
+```
+
+## Issues and Feedback
+
+For any other issues and feedback about this plugin, please submit it to this repository.
+
+## Troubleshooting
+
+If you have trouble using plugins, check out the [Plugins Troubleshooting](https://docs.fastlane.tools/plugins/plugins-troubleshooting/) guide.
+
+## Using _fastlane_ Plugins
+
+For more information about how the `fastlane` plugin system works, check out the [Plugins documentation](https://docs.fastlane.tools/plugins/create-plugin/).
+
+## About _fastlane_
+
+_fastlane_ is the easiest way to automate beta deployments and releases for your iOS and Android apps. To learn more, check out [fastlane.tools](https://fastlane.tools).
---
+++ lib/fastlane/plugin/android_emulator/actions/android_emulator_action.rb
@@ -0,0 +1,119 @@
+require 'fastlane/action'
+require_relative '../helper/android_emulator_helper'
+
+module Fastlane
+  module Actions
+    module SharedValues
+      ANDROID_SDK_DIR = :ANDROID_SDK_DIR
+    end
+
+    class AndroidEmulatorAction < Action
+      def self.run(params)
+        sdk_dir = params[:sdk_dir]
+        adb = "#{sdk_dir}/platform-tools/adb"
+
+        UI.message("Stopping emulator")
+        system("#{adb} emu kill > /dev/null 2>&1 &")
+
+        UI.message("Creating new emulator")
+        FastlaneCore::CommandExecutor.execute(
+          command: "#{sdk_dir}/tools/bin/avdmanager create avd -n '#{params[:name]}' -f -k '#{params[:package]}' -d 'Nexus 5'",
+          print_all: true,
+          print_command: false
+        )
+
+        UI.message("Override configuration")
+        open("#{Dir.home}/.android/avd/#{params[:name]}.avd/config.ini", 'a') { |f|
+          f << "hw.gpu.mode=auto\n"
+          f << "hw.gpu.enabled=yes\n"
+          f << "skin.dynamic=yes\n"
+          f << "skin.name=1080x1920\n"
+        }
+
+        # Verify HAXM installed on mac
+        if FastlaneCore::Helper.mac?
+          kextstat = Actions.sh("kextstat", log: false)
+
+          UI.user_error! "Please install the HAXM-Extension" unless kextstat.include?("intel")
+        end
+
+        UI.message("Starting emulator")
+        system("LC_NUMERIC=C; #{sdk_dir}/tools/emulator @#{params[:name]} > /dev/null 2>&1 &")
+        sh("#{adb} -e wait-for-device")
+
+        until Actions.sh("#{adb} -e shell getprop init.svc.bootanim", log: false).include? "stopped" do
+          sleep(5)
+        end
+
+        if params[:location]
+          UI.message("Set location")
+          sh("LC_NUMERIC=C; #{adb} emu geo fix #{params[:location]}")
+        end
+
+        if params[:demo_mode]
+          UI.message("Set in demo mode")
+          sh("#{adb} -e shell settings put global sysui_demo_allowed 1")
+          sh("#{adb} -e shell am broadcast -a com.android.systemui.demo -e command clock -e hhmm 0700")
+        end
+      end
+
+      def self.description
+        "Creates and starts an Android Emulator (AVD)"
+      end
+
+      def self.details
+        "Great for Screengrab"
+      end
+
+      def self.example_code
+        [
+          'android_emulator(
+            location: "9.1808 48.7771",
+            package: "system-images;android-24;google_apis;x86_64",
+            demo_mode: true,
+            sdk_dir: "PATH_TO_SDK"
+          )'
+        ]
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :sdk_dir,
+                                       env_name: "ANDROID_SDK_DIR",
+                                       description: "Path to the Android SDK DIR",
+                                       default_value: Actions.lane_context[SharedValues::ANDROID_SDK_DIR],
+                                       optional: false,
+                                       verify_block: proc do |value|
+                                         UI.user_error!("No ANDROID_SDK_DIR given, pass using `sdk_dir: 'sdk_dir'`") unless value and !value.empty?
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :package,
+                                      env_name: "AVD_PACKAGE",
+                                      description: "The selected system image of the emulator",
+                                      optional: false),
+          FastlaneCore::ConfigItem.new(key: :name,
+                                       env_name: "AVD_NAME",
+                                       description: "Name of the AVD",
+                                       default_value: "fastlane",
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :location,
+                                       env_name: "AVD_LOCATION",
+                                       description: "Set location of the the emulator '<longitude> <latitude>'",
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :demo_mode,
+                                       env_name: "AVD_DEMO_MODE",
+                                       description: "Set the emulator in demo mode",
+                                       is_string: false,
+                                       default_value: true)
+        ]
+      end
+
+      def self.authors
+        ["Michael Ruhl"]
+      end
+
+      def self.is_supported?(platform)
+        platform == :android
+      end
+    end
+  end
+end
