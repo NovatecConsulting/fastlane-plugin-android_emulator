61f8689c8b7e73604f71011a3a33ca4ef8d3b289
Michael Ruhl <michael.ruhl@novatec-gmbh.de>
2018-12-07 15:56:44 +0100

    Make the tests more readable

--- spec/android_emulator_action_spec.rb
+++ spec/android_emulator_action_spec.rb
@@ -1,16 +1,16 @@
 describe Fastlane::Actions::AndroidEmulatorAction do
-  describe '#run' do
+  describe 'Android-Emulator Integration' do
     it 'fails without sdk_dir' do
       expect do
         Fastlane::FastFile.new.parse("lane :test do
-            android_emulator({package: '23'})
+            android_emulator(package: '23')
           end").runner.execute(:test)
       end.to raise_error("No ANDROID_SDK_DIR given, pass using `sdk_dir: 'sdk_dir'`")
     end
     it 'fails without package' do
       expect do
         Fastlane::FastFile.new.parse("lane :test do
-            android_emulator({sdk_dir: '23'})
+            android_emulator(sdk_dir: '23')
           end").runner.execute(:test)
       end.to raise_error("No value found for 'package'")
     end
